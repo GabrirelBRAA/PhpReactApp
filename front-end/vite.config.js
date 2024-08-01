f3a48fd9b7d4d52dab971d2af397b8975fa25fd2
Gabriel <jgbiel29@gmail.com>
2024-07-31 23:50:49 -0300

    Modificdo algumas configurações para acessar a aplicação do front, infelizmente está ocorrendo algum leak de memória na aplicação do vite.

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	server: {
+		hmr: false,
+		host: true,
+		port: 5173,
+		followSymlinks: false,
+	}
 })
